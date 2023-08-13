ac4c480ce2374423e03e3ac1902f420b949fc69a
Orhun Aka <orhunaka@proton.me>
2023-08-13 19:59:22 +0300

    react fragments

--- src/index.js
+++ src/index.js
@@ -59,7 +59,6 @@ function App() {
 
 function Header() {
   const style = {};
-  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
 
   return (
     <header className="header">
@@ -77,11 +76,18 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later ;)</p>
       )}
